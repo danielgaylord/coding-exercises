a5d49cb310b2befde401d0df88b635ed6d88dce8
Daniel Gaylord <danielgaylord@gmail.com>
2021-10-18 21:27:18 -0400

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -1,15 +1,9 @@
-from itertools import product
+def diagonalDifference(arr):
+    slash, backslash = 0, 0
+    for row in range(len(arr)):
+        slash += arr[row][len(arr) - row - 1]
+        backslash += arr[row][row]
+    return abs(backslash - slash)
 
-K, mod = [int(n) for n in input().split()]
-all_narrs = []
-
-for lists in range(K):
-    all_narrs.append(list(map(lambda n: int(n) ** 2, input().split()[1:])))
-
-S = 0
-for cart in product(*all_narrs):
-    print(cart)
-    if (sum(cart) % mod) > S:
-        S = sum(cart)
-
-print(S)
+if __name__ == "__main__":
+    print(diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]]))
