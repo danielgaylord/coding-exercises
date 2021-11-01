22adb160d61462648cc088b98d186c94b36731e4
Daniel Gaylord <danielgaylord@gmail.com>
2021-10-31 23:36:10 -0400

    Create string_validators.py

---
+++ hackerRank/string_validators.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    s = input()
+    alnum, alpha, digit, lower, upper = False, False, False, False, False
+    for char in s:
+        if char.isalnum():
+            alnum = True
+        if char.isalpha():
+            alpha = True
+        if char.isdigit():
+            digit = True
+        if char.islower():
+            lower = True
+        if char.isupper():
+            upper = True
+    print(alnum)
+    print(alpha)
+    print(digit)
+    print(lower)
+    print(upper)
