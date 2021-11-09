34666ad47afff7a3e0cac5a2ce58951ed822cb8e
Daniel Gaylord <danielgaylord@gmail.com>
2021-11-08 22:36:09 -0500

    Update num_valid_words.py

--- LeetCode/num_valid_words.py
+++ LeetCode/num_valid_words.py
@@ -23,9 +23,34 @@ class Solution(object):
 """
 
 class Solution(object):
+    def bitMask(self, word):
+        mask = 0
+        for char in word:
+            i = ord(char) - ord('a')
+            mask |= 1 << i
+        return mask
+    
     def findNumOfValidWords(self, words, puzzles):
         """
         :type words: List[str]
         :type puzzles: List[str]
         :rtype: List[int]
         """
+        freq = {}
+        result = [0 for _ in range(len(puzzles))]
+        for word in words:
+            mask = self.bitMask(word)
+            freq[mask] = freq.get(mask, 0) + 1
+        for index, puzzle in enumerate(puzzles):
+            mask = self.bitMask(puzzle)
+            submask = mask
+            total = 0
+            firstbitdex = ord(puzzle[0]) - ord('a')
+            while True:
+                if submask >> firstbitdex & 1:
+                    total += freq.get(submask, 0)
+                if submask == 0:
+                    break
+                submask = (submask - 1) & mask
+            result[index] = total
+        return result
