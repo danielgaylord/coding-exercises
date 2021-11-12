7a9e72596b96af362bdfe6422c0ce650192e2c60
Daniel Gaylord <danielgaylord@gmail.com>
2021-11-11 21:11:57 -0500

    Create BST-Insert.py

---
+++ hackerRank/BST-Insert.py
@@ -0,0 +1,28 @@
+#Node is defined as
+#self.left (the left child of the node)
+#self.right (the right child of the node)
+#self.info (the value of the node)
+
+    def insert(self, val):
+        #Enter you code here.
+        node = self.root
+
+        if not node:
+            self.root = Node(val)
+            return self.root
+            
+        while node:
+            if val < node.info:
+                if node.left:
+                    node = node.left
+                else:
+                    node.left = Node(val)
+                    break
+            else:
+                if node.right:
+                    node = node.right
+                else:
+                    node.right = Node(val)
+                    break
+        
+        return self.root
