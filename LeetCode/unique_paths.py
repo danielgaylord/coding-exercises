efda8bdbf63b71496d94660fd21fb80417427adf
Daniel Gaylord <danielgaylord@gmail.com>
2021-11-16 21:23:30 -0500

    Update unique_paths.py

--- LeetCode/unique_paths.py
+++ LeetCode/unique_paths.py
@@ -1,5 +1,4 @@
-from collections import deque
-
+# BFS Solution
 class Solution(object):
     def uniquePaths(self, m, n):
         """
@@ -12,3 +11,23 @@ class Solution(object):
             for y in range(1, n):
                 dp[x][y] = dp[x - 1][y] + dp[x][y - 1]
         return dp[m - 1][n - 1]
+
+# DFS Solution
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0 for c in range(n)] for r in range(m)]
+        
+        def helper(row, col):
+            if row >= m or col >= n:
+                return 0
+            if row == m - 1 and col == n - 1:
+                return 1
+            if dp[row][col] == 0:
+                dp[row][col] = helper(row + 1, col) + helper(row, col + 1)
+            return dp[row][col]
+        return helper(0, 0)
