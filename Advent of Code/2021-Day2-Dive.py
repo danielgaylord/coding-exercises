6e601c8d837e9e8e2aea8d2d76d156d75c859f10
Daniel Gaylord <danielgaylord@gmail.com>
2021-12-03 22:24:14 -0500

    Advent of Code updates

--- Advent of Code/2021-Day2-Dive.py
+++ Advent of Code/2021-Day2-Dive.py
@@ -1,26 +1,29 @@
 def dive():
     moves = []
-    while True:
-        line = input()
-        if line:
-            moves.append(line.split(" "))
-        else:
-            break
+    with open('Advent of Code/2021-Day2-Dive.txt', 'r') as input:
+        moves = [line.rstrip().split(" ") for line in input]
+
+    print(moves)
     
     horiz = 0
     depth = 0
+    new_depth = 0
     aim = 0
 
     for move in moves:
         if move[0] == "forward":
             horiz += int(move[1])
-            depth += aim * int(move[1])
+            new_depth += aim * int(move[1])
         elif move[0] == "down":
+            depth += int(move[1])
             aim += int(move[1])
         elif move[0] == "up":
+            depth -= int(move[1])
             aim -= int(move[1])
 
-    return horiz*depth
+    first_reading = horiz * depth
+    second_reading = horiz * new_depth
+    return second_reading
 
 if __name__ == "__main__":
     print(dive())
