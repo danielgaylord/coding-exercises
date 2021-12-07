b82bea096598a8053378e46751a95026da72e05d
Daniel Gaylord <danielgaylord@gmail.com>
2021-12-06 22:14:53 -0500

    LeetCode Daily - 12-6

---
+++ LeetCode/convert_bin_num_in_linklist_to_int.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        binary = ""
+        node = head
+        while node:
+            binary += str(node.val)
+            node = node.next
+            
+        return int(binary, 2)
