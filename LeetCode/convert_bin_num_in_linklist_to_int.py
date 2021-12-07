53e730195f9f44dc2daee42ce3069ccb80e1fed3
Daniel Gaylord <danielgaylord@gmail.com>
2021-12-06 22:27:33 -0500

    Update convert_bin_num_in_linklist_to_int.py

--- LeetCode/convert_bin_num_in_linklist_to_int.py
+++ LeetCode/convert_bin_num_in_linklist_to_int.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# V1: String
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
         binary = ""
@@ -12,3 +14,27 @@ class Solution:
             node = node.next
             
         return int(binary, 2)
+
+# V2: Math
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        node = head
+        binary = 0
+        while node:
+            binary *= 2
+            binary += node.val
+            node = node.next
+            
+        return binary
+
+#V3: Bitwise
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        node = head
+        binary = 0
+        while node:
+            binary <<= 1
+            binary |= node.val
+            node = node.next
+            
+        return binary
