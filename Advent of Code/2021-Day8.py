6b85ca18ecaa12cb1c3c9a1aaa3c7d0bfacd8d3a
Daniel Gaylord <danielgaylord@gmail.com>
2021-12-08 16:30:35 -0500

    Update 2021-Day8.py

--- Advent of Code/2021-Day8.py
+++ Advent of Code/2021-Day8.py
@@ -2,11 +2,17 @@ def seven_segment_search(patterns):
     digit_count = [0 for _ in range(10)]
     output_sum = 0
     for pattern in patterns:
+
+        # Find out which sequence represents which digit
         digit_rep = ["" for _ in range(10)]
+
+        # Easy representations based solely on length
         digit_rep[1] = "".join(sorted(pattern[0][0]))
         digit_rep[4] = "".join(sorted(pattern[0][2]))
         digit_rep[7] = "".join(sorted(pattern[0][1]))
         digit_rep[8] = "".join(sorted(pattern[0][-1]))
+        
+        # Difficult representations look at how many characters are left when removing the characters from each easy representation, for 8 the math is 'flipped'
         for i in range(3, 9):
             minus1, minus4, minus7, minus8 = pattern[0][i], pattern[0][i], pattern[0][i], digit_rep[8]
             for char in digit_rep[1]:
@@ -17,6 +23,7 @@ def seven_segment_search(patterns):
                 minus7 = minus7.replace(char, "")
             for char in pattern[0][i]:
                 minus8 = minus8.replace(char, "")
+
             if len(minus1) == 4 and len(minus4) == 3 and len(minus7) == 3 and len(minus8) == 1:
                 digit_rep[0] = "".join(sorted(pattern[0][i]))
             elif len(minus1) == 4 and len(minus4) == 3 and len(minus7) == 3 and len(minus8) == 2:
@@ -29,30 +36,27 @@ def seven_segment_search(patterns):
                 digit_rep[6] = "".join(sorted(pattern[0][i]))
             elif len(minus1) == 4 and len(minus4) == 2 and len(minus7) == 3 and len(minus8) == 1:
                 digit_rep[9] = "".join(sorted(pattern[0][i]))
+        
+        # Loop through each output digit to form the 4-digit number (and count how many of each digit for part 1)
         output = 0
         for digit in pattern[1]:
             output *= 10
-            output += digit_rep.index("".join(sorted(digit)))
-                
-        output_sum += output
+            value = digit_rep.index("".join(sorted(digit)))
+            output += value
+            digit_count[value] += 1
 
-    return output_sum
+        # easy_sum is for part 1, output_sum is for part 2       
+        output_sum += output
+    easy_sum = digit_count[1] + digit_count[4] + digit_count[7] + digit_count[8]
+
+    return easy_sum
 
 if __name__ == "__main__":
     patterns = []
     with open('Advent of Code/2021-Day8.txt', 'r') as input:
         temp = [line.rstrip().split(" | ") for line in input]
         for line in temp:
-            hold = []
-            hold.append(line[0].split(" "))
-            hold.append(line[1].split(" "))
-            hold[0] = list(sorted(hold[0], key=len))
-            patterns.append(hold)
-
-    check = "abc"
-    digit = "ac"
-    for char in digit:
-        check = check.replace(char, "")
+            patterns.append([list(sorted(line[0].split(" "), key=len)), line[1].split(" ")])
     
     print(seven_segment_search(patterns))
         
