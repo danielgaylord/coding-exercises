c082cc8f1183636b04f8b72a9c6f2be4fa40da71
Daniel Gaylord <danielgaylord@gmail.com>
2021-12-08 23:18:29 -0500

    LeetCode Daily 12-8

--- LeetCode/jump_game_III.py
+++ LeetCode/jump_game_III.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+# Visited List
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         queue = deque()
@@ -16,5 +17,27 @@ class Solution:
             if 0 <= jump_left and not visited[jump_left]:
                 queue.append(jump_left)
             if jump_right < len(arr) and not visited[jump_right]:
+                queue.append(jump_right)
+        return False
+
+#Visited Set
+from collections import deque
+
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        queue = deque()
+        queue.append(start)
+        visited = set()
+        
+        while queue:
+            loc = queue.popleft()
+            visited.add(loc)
+            if arr[loc] == 0:
+                return True
+            jump_left = loc - arr[loc]
+            jump_right = loc + arr[loc]
+            if 0 <= jump_left and jump_left not in visited:
+                queue.append(jump_left)
+            if jump_right < len(arr) and jump_right not in visited:
                 queue.append(jump_right)
         return False
