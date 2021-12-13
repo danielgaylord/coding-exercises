4a2aa6ae569fd508b60eb5196f954d44cf86a8b1
Daniel Gaylord <danielgaylord@gmail.com>
2021-12-12 21:25:21 -0500

    Update 2021-Day12.py

--- Advent of Code/2021-Day12.py
+++ Advent of Code/2021-Day12.py
@@ -1,23 +1,32 @@
 from collections import defaultdict
 
 def passage_pathing(cave_map):
-       
+    
+    # Recursive depth-first search
     def dfs(cave_map, visited, loc, visited_small):
+        
+        # Keep track of which nodes have been visited and how many times
+        # If just doing part 1, you only need to add the visited node to
+        # the dict or use a set instead
         new_visited = visited.copy()
         if loc == "end":
             return 1
-        if loc.islower():
-            if loc in new_visited:
-                new_visited[loc] += 1
-            else:
-                new_visited[loc] = 1
+        if loc in new_visited:
+            new_visited[loc] += 1
+        else:
+            new_visited[loc] = 1
 
+        # For every node that the current location points to...
+        # If going to an upper case node, just go, no worries
+        # If going to a lower case node, make sure it's not visited already
         paths = 0
         for to in cave_map[loc]:
             if to.isupper():
                 paths += dfs(cave_map, new_visited, to, visited_small)
             elif to not in new_visited:
                 paths += dfs(cave_map, new_visited, to, visited_small)
+            # For part 2 you can also go to a lower case node if you haven't been
+            # twice already or to another node twice
             elif to in new_visited and new_visited[to] < 2 and not visited_small:
                 paths += dfs(cave_map, new_visited, to, True)
         
@@ -28,9 +37,13 @@ def passage_pathing(cave_map):
 
 if __name__ == "__main__":
     cave_map = defaultdict(list)
-    with open('Advent of Code/2021-Day12.txt', 'r') as input:
+    with open('Advent of Code/test.txt', 'r') as input:
         for line in input:
             connection = line.rstrip().split("-")
+
+            # Create a map of every left node to every right node an vise versa
+            # If one of the nodes is start, only keep track of what start goes to
+            # Similarly if one node is end, only keep track of what goes to end
             if connection[1] != "start" and connection[0] != "end":
                 cave_map[connection[0]].append(connection[1])
             if connection[0] != "start" and connection[1] != "end":
