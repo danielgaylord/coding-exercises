a02b3769cd67c3639d0a93d56572a7bae11c6c38
Dan Gaylord <danielgaylord@gmail.com>
2022-01-21 19:29:42 -0500

    LeetCode weekly
    January week 4

---
+++ LeetCode/find_nearest_right_node_in_binary_tree.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findNearestRightNode(self, root: TreeNode, u: TreeNode) -> Optional[TreeNode]:
+        queue = deque()
+        queue.append(root)
+        
+        while queue:
+            size = len(queue)
+            for x in range(size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if node == u and x < size - 1:
+                    return queue.popleft()
+        return None
