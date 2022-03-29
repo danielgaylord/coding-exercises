2075ae96d6adbf3ea4c8c35976c8b33f00198027
Dan Gaylord <danielgaylord@gmail.com>
2022-03-28 21:40:37 -0400

    LeetCode daily
    3/29/2022

---
+++ LeetCode/find_dup_num.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        fast = slow = nums[0]
+        
+        while True:
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+            if fast == slow:
+                break
+        
+        slow = nums[0]
+        while fast != slow:
+            fast = nums[fast]
+            slow = nums[slow]
+        
+        return fast
